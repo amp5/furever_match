542ea53dac04d1c6197bdb291d27987c1fcddbd0
amp5 <aplassaras@gmail.com>
2020-02-06 15:57:49 -0800

    have working dash app with two viz

--- src/dash_app.py
+++ src/dash_app.py
@@ -3,12 +3,14 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
 from access_db import *
 import plotly.express as px
 from dash.dependencies import Input, Output
+import plotly.graph_objs as go
 
 
-server = flask.Flask(__name__)
+server = flask.Flask(__name__)ß
 app = dash.Dash(
     __name__,
     server=server,
@@ -17,50 +19,63 @@ app = dash.Dash(
     )
 
 
-navbar_children = [
-    dbc.NavItem(dbc.NavLink("Page 1", href="/page-1")),
-    dbc.NavItem(dbc.NavLink("Page 2", href="/page-2")),
-]
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Analysts", href="#coats_color")),
+        dbc.NavItem(dbc.NavLink("Engineers", href="#missing_data")),
+    ],
+    brand="Furever Match",
+    brand_href="#",
+    sticky="top",
+)
 
-navbar = dbc.Navbar(navbar_children, sticky="top")
+query1 = """select coat, colors_primary, count(*) from animal_info where coat is not NULL and colors_primary is not NULL group by coat, colors_primary;"""
+analyst_df = run_query(query1)
 
-query = """select coat, count(*) from animal_info group by coat;"""
-df = run_query(query)
+
+
+query2 = """select raw.*, round(raw.num_null / raw.total_rows , 2) as pct_null  from (select *, (select count(*) as total_rows from animal_temperment) from
+((select 'dogs' as column_name,  count(*) as num_null from animal_temperment where dogs is null ) union
+    (select 'id' as column_name,  count(*) as num_null from animal_temperment where id is null ) union
+    (select 'children' as column_name,  count(*) as num_null from animal_temperment where children is null ) union
+    (select 'cats' as column_name,  count(*) as num_null from animal_temperment where cats is null )) nulls) raw;"""
+engineer_df = run_query(query2)
+
+
+hovertemplate = "<b>  %{y} and %{x} : %{z} Cats "
 
 app.layout = html.Div(children=[
-    html.H1(children='Dash Tutorials'),
+    navbar,
     dcc.Graph(
-        id='example1',
-        figure={
-            'data': [
-                {'x': [1, 2, 3, 4, 5], 'y': [9, 6, 2, 1, 5], 'type': 'line', 'name': 'Boats'},
-                {'x': [1, 2, 3, 4, 5], 'y': [8, 7, 2, 7, 3], 'type': 'bar', 'name': 'Cars'},
-            ],
-            'layout': {
-                'title': 'Basic Dash Example'
+            id='coats_color',
+            figure={
+                'data': [
+                    go.Heatmap(
+                        z=analyst_df['count'],
+                        x=analyst_df['colors_primary'],
+                        y=analyst_df['coat'],
+                        colorscale='Viridis',
+                        hoverongaps=False,
+                    hovertemplate=hovertemplate)
+                ],
+                'layout': go.Layout(
+                    xaxis={'title': 'Coat Color Variations'},
+                    yaxis={'title': 'Coat Length Variations'},
+                    hovermode='closest'
+                )
             }
-        }
-
     ),
-dcc.Input(id='input', value='Enter Table name', type ='text'),
-    html.Div(id='output')
-])
+    dash_table.DataTable(
+        id='table',
+        columns=[{"name": i, "id": i} for i in engineer_df.columns],
+        data=engineer_df.to_dict('records'),
+    ),
 
-# @app.callback(
-#     Output(component_id='output', component_property='children'),
-#     [Input(component_id='input', component_property='value')])
-# def update_value(input_data):
-#     return "Input: {}".format(input_data)
+],
+style={'padding': '10px 40px 40px 40px'})
 
 
-# fig = go.Figure(data=go.Heatmap(
-#                    z=[[1, None, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, -10, 20]],
-#                    x=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-#                    y=['Morning', 'Afternoon', 'Evening'],
-#                    hoverongaps = False))
-
-fig = px.bar(df, x="coat", y="count", barmode="group")
-fig.show()
+d = [{"name": i, "id": i} for i in dft.columns]
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0', port=8080)
