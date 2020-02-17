c3c2ef9b1230e9a6f305efa5ca36f5633f34d0ab
amp5 <aplassaras@gmail.com>
2020-02-16 22:51:24 -0800

    updated front end with new table and broken out queries

--- client/dash_app.py
+++ client/dash_app.py
@@ -18,137 +18,21 @@ app = dash.Dash(
     external_stylesheets=[dbc.themes.BOOTSTRAP]
 )
 
-navbar = dbc.NavbarSimple(
-    children=[
-        dbc.NavItem(dcc.Link('Analysts   ', href='/page-1')),
-        dbc.NavItem(dcc.Link('   Engineers', href='/page-2')),
-    ],
-    brand="Furever Match",
-    brand_href="#",
-    sticky="top",
-)
-
-
-
-
-
-app.config.suppress_callback_exceptions = True
-
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
-])
-
 
 app.layout = html.Div([
     dcc.Tabs(id="tabs", value='tab-1', children=[
-        dcc.Tab(label='Tab one', value='tab-1'),
-        dcc.Tab(label='Tab two', value='tab-2'),
+        dcc.Tab(label='Analysis Dashboard', value='tab-1'),
+        dcc.Tab(label='Engineering Dashboard', value='tab-2'),
     ]),
     html.Div(id='tabs-content')
 ])
 
-@app.callback(Output('tabs-content', 'children'),
-              [Input('tabs', 'value')])
-def render_content(tab):
-    if tab == 'tab-1':
-        return html.Div([
-            html.H3('Tab content 1'),
-            html.H4('yo')
-        ])
-    elif tab == 'tab-2':
-        return html.Div([
-            html.H3('Tab content 2')
-        ])
-
-
-
-index_page = html.Div([
-    dcc.Link('Analysts', href='/page-1'),
-    html.Br(),
-    dcc.Link('Engineers', href='/page-2'),
-])
-
-query1 = get_data("coat_variations")
-# query1 = """select animal_info.coat, animal_info.colors_primary, count(*) as num from animal_info where animal_info.coat is not NULL and animal_info.colors_primary is not NULL group by animal_info.coat, animal_info.colors_primary;"""
-analyst_df = run_query(query1)
+analyst_df = run_query(get_data("coat_variations"))
 sorted_analyst = analyst_df.sort_values(by='colors_primary')
 hovertemplate1 = "<b>  %{y} and %{x} : %{z} Cats "
 
-#### Shelter query
-shelter_q = """
-                select 
-                    organization_name,
-                    num_declawed,
-                    num_not_house_trained,
-                    num_shots_not_current,
-                    num_special_needs,
-                    num_not_fixed
-                from
-                    (
-                        select
-                            declaw.organization_id,
-                            num_declawed,
-                            num_not_house_trained,
-                            num_shots_not_current,
-                            num_special_needs,
-                            num_not_fixed
-                        from
-                            (select
-                                organization_id,
-                                count(declawed) as num_declawed
-                            from animal_medical_info
-                            left join animal_info
-                                on animal_info.id = animal_medical_info.id
-                            where declawed is True
-                            group by organization_id) declaw
 
-                        left join
-                            (select
-                                organization_id,
-                                count(house_trained) as num_not_house_trained
-                            from animal_medical_info
-                            left join animal_info
-                                on animal_info.id = animal_medical_info.id
-                            where house_trained is False
-                            group by organization_id) house
-                        on declaw.organization_id = house.organization_id
-                        left join
-                            (select
-                                organization_id,
-                                count(shots_current) as num_shots_not_current
-                            from animal_medical_info
-                            left join animal_info
-                                on animal_info.id = animal_medical_info.id
-                            where shots_current is False
-                            group by organization_id) shots
-                        on declaw.organization_id = shots.organization_id
-                        left join
-                            (select
-                                organization_id,
-                                count(special_needs) as num_special_needs
-                            from animal_medical_info
-                            left join animal_info
-                                on animal_info.id = animal_medical_info.id
-                            where special_needs is True
-                            group by organization_id) special
-                        on declaw.organization_id = special.organization_id
-                        left join
-                            (select
-                                organization_id,
-                                count(spayed_neutered) as num_not_fixed
-                            from animal_medical_info
-                            left join animal_info
-                                on animal_info.id = animal_medical_info.id
-                            where spayed_neutered is False
-                            group by organization_id) fixed
-                        on declaw.organization_id = fixed.organization_id
-                    ) main
-                left join organization_info
-                on main.organization_id = organization_info.organization_id
-                ;
-            """
-shelter_df = run_query(shelter_q)
+shelter_df = run_query(get_data("shelter_med"))
 shelter_df['total'] = shelter_df['num_declawed'] + \
                       shelter_df['num_not_house_trained'] + \
                       shelter_df['num_shots_not_current'] + \
@@ -188,43 +72,15 @@ stacked_layout = go.Layout(
 stacked_fig = go.Figure(data=stacked_d, layout=stacked_layout)
 
 
-def table_selection(table):
-    if table == 'temp':
-        query_selection = """
-                select 
-                    *, 
-                    (
-                        select 
-                            count(*) as total_rows 
-                        from animal_temperment
-                    ) 
-                from
-                    (
-                        (select 'dogs' as column_name,  count(*) as num_null from animal_temperment where dogs is null) 
-                        union
-                        (select 'id' as column_name,  count(*) as num_null from animal_temperment where id is null ) 
-                        union
-                        (select 'children' as column_name,  count(*) as num_null from animal_temperment where children is null ) 
-                        union
-                        (select 'cats' as column_name,  count(*) as num_null from animal_temperment where cats is null )
-                    ) nulls;"""
-    engineer_df = run_query(query_selection)
-    engineer_df['Percent Null'] = (engineer_df['num_null'] / engineer_df['total_rows'] * 100).round(2)
 
-    clean_eng = engineer_df[['column_name', 'num_null', 'Percent Null']].copy()
-    clean_eng.columns = ["Column Name", "Number of Null Values", 'Percent Null']
-    return clean_eng
-
-
-table_df = table_selection('temp')
+nulls = get_data('null')
+engineer_df = run_query(nulls)
+engineer_df['Percent Null'] = (engineer_df['num_null'] / engineer_df['total_rows'] * 100).round(2)
+clean_eng = engineer_df[['table_name', 'column_name', 'num_null', 'Percent Null']].copy()
+clean_eng.columns = ["Table Name", "Column Name", "Number of Null Values", 'Percent']
+sorted_clean = clean_eng.sort_values('Percent', ascending=False)
 
 page_1_layout = html.Div(children=[
-    navbar,
-    html.H1('Analysis Dashboard',
-            style={
-                'textAlign': 'center',
-            }
-            ),
     html.Div([
         html.H4('Shelter Level',
                 style={
@@ -270,36 +126,17 @@ page_1_layout = html.Div(children=[
             }
         ),
     ],
-        style={'width': '80%', 'display': 'inline-block', 'vertical-align': 'right'}
+        style={'width': '80%', 'display': 'inline-block', 'vertical-align': 'right', 'padding': '10%'}
     ),
 ]
 )
 
 page_2_layout = html.Div(children=[
-    navbar,
-    html.H1('Engineering Dashboard',
-            style={
-                'textAlign': 'center',
-            }
-            ),
-    html.Br(),
-    html.H4('Select Table'),
-    dcc.Dropdown(id='table_dropdown',
-                 options=[
-                     {'label': 'Medical Information', 'value': 'med'},
-                     {'label': 'Animal Information', 'value': 'ani'},
-                     {'label': 'Temperment Information', 'value': 'temp'},
-                     {'label': 'Status Information', 'value': 'stat'},
-                     {'label': 'Animal Description', 'value': 'descr'}
-                 ],
-                 value='temp'
-                 ),
-
-    html.Br(),
+    html.H3('Missing values'),
     dash_table.DataTable(
         id='table1',
-        columns=[{"name": i, "id": i} for i in table_df.columns],
-        data=table_df.to_dict('records'),
+        columns=[{"name": i, "id": i} for i in sorted_clean.columns],
+        data=sorted_clean.to_dict('records'),
         sort_action="native",
         style_cell={'textAlign': 'center'},
         style_header={
@@ -310,46 +147,36 @@ page_2_layout = html.Div(children=[
             {
                 'if': {'column_id': 'Column Name'},
                 'textAlign': 'left'
-            }
+            },
+            {
+                'if': {'column_id': 'Table Name'},
+                'textAlign': 'left'
+            },
+            # {
+            #     'if': {
+            #         'column_id': 'Percent',
+            #         'filter_query': '{Percent} > 50'
+            #     },
+            #     'backgroundColor': '#99493d',
+            #     'color': 'white',
+            # },
         ],
 
     ),
-
-
-    dcc.Link('Go back to home', href='/')
 ],
-    # style={'padding': '10% 10% 10% 10%'}
+ style={'padding': '10%'}
 )
 
 
-@app.callback(dash.dependencies.Output('table1', 'figure'),
-              [dash.dependencies.Input('table_dropdown', 'value')])
-def update_output(table_opt):
-    return table_opt
-
-
-@app.callback(dash.dependencies.Output('page-1-content', 'children'),
-              [dash.dependencies.Input('page-1-dropdown', 'value')])
-def page_1_dropdown(value):
-    return 'You have selected "{}"'.format(value)
-
-
-@app.callback(dash.dependencies.Output('page-2-content', 'children'),
-              [dash.dependencies.Input('page-2-radios', 'value')])
-def page_2_radios(value):
-    return 'You have selected "{}"'.format(value)
-
-
-# Update the index
-@app.callback(dash.dependencies.Output('page-content', 'children'),
-              [dash.dependencies.Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/page-1':
-        return page_1_layout
-    elif pathname == '/page-2':
-        return page_2_layout
-    else:
-        return index_page
+@app.callback(Output('tabs-content', 'children'),
+              [Input('tabs', 'value')])
+def render_content(tab):
+    if tab == 'tab-1':
+        return html.Div(page_1_layout)
+    elif tab == 'tab-2':
+        return html.Div([
+            page_2_layout
+        ])
 
 
 if __name__ == '__main__':
--- client/queries.py
+++ client/queries.py
@@ -1,4 +1,3 @@
-
 def get_data(request):
     if request == 'coat_variations':
 
@@ -89,9 +88,156 @@ def get_data(request):
             """
 
         return query
+    if request == 'null_temp':
+        query = """
+                select
+                    *,
+                    (
+                        select
+                            count(*) as total_rows
+                        from animal_temperment
+                    )
+                from
+                    (
+                        (select 'dogs' as column_name,  count(*) as num_null from animal_temperment where dogs is null)
+                        union
+                        (select 'id' as column_name,  count(*) as num_null from animal_temperment where id is null )
+                        union
+                        (select 'children' as column_name,  count(*) as num_null from animal_temperment where children is null )
+                        union
+                        (select 'cats' as column_name,  count(*) as num_null from animal_temperment where cats is null )
+                    ) nulls;"""
+        return query
+    if request == 'null':
+        query = """
+                    (
+                         select
+                         'animal_temperment' as table_name,
+                            *,
+                            (
+                                select
+                                    count(*) as total_rows
+                                from animal_temperment
+                            )
+                        from
+                            (
+                                (select 'dogs' as column_name,  count(*) as num_null from animal_temperment where dogs is null)
+                                union
+                                (select 'id' as column_name,  count(*) as num_null from animal_temperment where id is null )
+                                union
+                                (select 'children' as column_name,  count(*) as num_null from animal_temperment where children is null )
+                                union
+                                (select 'cats' as column_name,  count(*) as num_null from animal_temperment where cats is null )
+                            ) nulls
+                    )
+                    union
+                    (
+                         select
+                                 'animal_medical_info' as table_name,
+                            *,
+                            (
+                                select
+                                    count(*) as total_rows
+                                from animal_medical_info
+                            )
+                        from
+                            (
+                                (select 'declawed' as column_name,  count(*) as num_null from animal_medical_info where declawed is null)
+                                union
+                                (select 'id' as column_name,  count(*) as num_null from animal_medical_info where id is null )
+                                union
+                                (select 'house_trained' as column_name,  count(*) as num_null from animal_medical_info where house_trained is null )
+                                union
+                                (select 'shots_current' as column_name,  count(*) as num_null from animal_medical_info where shots_current is null )
+                                union
+                                (select 'special_needs' as column_name,  count(*) as num_null from animal_medical_info where special_needs is null )
+                                union
+                                (select 'spayed_neutered' as column_name,  count(*) as num_null from animal_medical_info where spayed_neutered is null )
+                            ) nulls
+                    )
+                    union
+                    (
+                        select
+                             'animal_info' as table_name,
+                            *,
+                            (
+                                select
+                                    count(*) as total_rows
+                                from animal_info
+                            )
+                        from
+                            (
+                                (select 'organization_id' as column_name,  count(*) as num_null from animal_info where organization_id is null)
+                                union
+                                (select 'id' as column_name,  count(*) as num_null from animal_info where id is null )
+                                union
+                                (select 'name' as column_name,  count(*) as num_null from animal_info where animal_info.name is null )
+                                union
+                                (select 'size' as column_name,  count(*) as num_null from animal_info where animal_info.size is null )
+                                union
+                                (select 'age' as column_name,  count(*) as num_null from animal_info where age is null )
+                                union
+                                (select 'gender' as column_name,  count(*) as num_null from animal_info where gender is null )
+                                union
+                                (select 'breeds_primary' as column_name,  count(*) as num_null from animal_info where breeds_primary is null )
+                                union
+                                (select 'breeds_secondary' as column_name,  count(*) as num_null from animal_info where breeds_secondary is null )
+                                union
+                                (select 'breeds_mixed' as column_name,  count(*) as num_null from animal_info where breeds_mixed is null )
+                                union
+                                (select 'breeds_unknown' as column_name,  count(*) as num_null from animal_info where breeds_unknown is null )
+                                union
+                                (select 'colors_primary' as column_name,  count(*) as num_null from animal_info where colors_primary is null )
+                                union
+                                (select 'colors_secondary' as column_name,  count(*) as num_null from animal_info where colors_secondary is null )
+                                union
+                                (select 'colors_mixed' as column_name,  count(*) as num_null from animal_info where colors_mixed is null )
+                                union
+                                (select 'coat' as column_name,  count(*) as num_null from animal_info where coat is null )
+                                union
+                                (select 'published_at' as column_name,  count(*) as num_null from animal_info where published_at is null )
+                            ) nulls
+                    )
+                    union
+                    (
+                         select
+                            'animal_description' as table_name,
+                            *,
+                            (
+                                select
+                                    count(*) as total_rows
+                                from animal_description
+                            )
+                        from
+                            (
+                                (select 'description' as column_name,  count(*) as num_null from animal_description where description is null)
+                                union
+                                (select 'id' as column_name,  count(*) as num_null from animal_description where id is null )
+                            ) nulls
+                    )
+                    union
+                    (
+                        select
+                                'animal_status' as table_name,
+                            *,
+                            (
+                                select
+                                    count(*) as total_rows
+                                from animal_status
+                            )
+                        from
+                            (
+                                (select 'status' as column_name,  count(*) as num_null from animal_status where status is null)
+                                union
+                                (select 'id' as column_name,  count(*) as num_null from animal_status where id is null )
+                                union
+                                (select 'status_changed_at' as column_name,  count(*) as num_null from animal_status where status_changed_at is null )
+                            ) nulls
+                    );
+                    """
+        return query
 
 
     else:
         raise Exception('Your request is not currently supported')
-
 
